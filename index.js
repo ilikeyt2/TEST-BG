ee509d8ea7501e958c4cf36e198a0d4bbc5489f7
ilikeyt2 <walerious2023@gmail.com>
2024-10-19 14:22:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
 console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["", ""];
+const statusMessages = ["Subscribe to","Walerious"];
 let currentIndex = 0;
 
 async function login() {
