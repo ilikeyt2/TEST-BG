9d31400f8b92f92ba34f730e89a66b4e10b3119d
ilikeyt2 <walerious2023@gmail.com>
2024-10-19 13:31:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🚀 Improving products.", "🚀 Looking for something to do", "🚀 Loading files", "🚀 Reading the suggestions", "🚀 Testing Products"];
+const statusMessages = [""
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
