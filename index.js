69b452eaf105ce399058f812a89d1fe333efe60f
ilikeyt2 <walerious2023@gmail.com>
2024-11-04 17:37:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,10 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
-let currentStatusIndex = 0;
-let currentTypeIndex = 0;
+const statusMessages = ["Subcribe to", "Walerious"];
 
 async function login() {
   try {
